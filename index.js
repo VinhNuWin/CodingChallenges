2e3e3d68e5b5c886dd14a3bbc6eb2f469e8f39aa
Vinh Nguyen <vinhnuwin@protonmail.com>
2022-08-23 13:03:52 -0700

    codewars 8/23 push

--- index.js
+++ index.js
@@ -7,3 +7,22 @@ function boolToWord( bool ){
       return "No"
     }
   }
+
+  // Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+  function descendingOrder(n){
+    let newN = n.toString().split('').sort(function(a, b){ return b-a;}).join('');
+    return +newN
+  }
+
+  // Twice as old, calculate how many years ago the father was twice as old as his son (or in how many years he will be twice as old)
+
+  function twiceAsOld(dadYearsOld, sonYearsOld){
+    return Math.abs(dadYearsOld - 2 * sonYearsOld);
+  }
+
+  // Parse nice int from char problem
+
+  function getAge(inputString){
+    return parseInt(inputString);
+  }
