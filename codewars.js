2998f8beb8461f56232f163b036cd718e6180298
Vinh Nguyen <vinhnuwin@protonmail.com>
2023-01-03 11:10:41 -0800

    Strings Challenge

--- codewars.js
+++ codewars.js
@@ -352,3 +352,39 @@ function order(words){
   r.sort((a,b)=>a[1]-b[1]).map(x=> x.splice(1,1))
   return r.join(' ')
 }
+
+// Convert an array of strings to array of numbers
+
+function toNumberArray(stringarray){
+
+  return stringarray.map(parseFloat);
+}
+
+// break camelCase
+
+function solution(string) {
+  return string.replace(/[A-Z]/g, " $&");
+}
+
+console.log('camelCasing: ', solution('camelCasing'));
+console.log('camelCasingTest: ', solution('camelCasingTest'));
+
+// Consecutive Strings
+
+function longestConsec(strarr, k) {
+  let arrLength = strarr.length;
+  let arr = [];
+  
+  if(arrLength == 0 || k > arrLength || k <= 0) return "";
+    
+  for(let i = 0; i <= arrLength - k; i++){
+    let current = strarr[i];
+    for(let ii = k, jj = 1; ii > 1; ii--, jj++){
+      current += strarr[i+jj]
+    }
+  arr.push(current);
+  }
+ return arr.reduce(function (a, b) {
+        return a.length > b.length ? a : a.length == b.length ? a : b;
+    });
+}
