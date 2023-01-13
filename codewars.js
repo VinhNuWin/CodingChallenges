f9e4ca46b83a5ba190a093a925cc272430cdc283
Vinh Nguyen <vinhnuwin@protonmail.com>
2023-01-12 23:27:56 -0800

    6kyu persistent bugger

--- codewars.js
+++ codewars.js
@@ -1,31 +1,3 @@
-// 6kyu-persistent-bugger
-
-
-
-function persistence(num) {
-
-  if (num.toString().length === 1) {
-
-    return 0;
-
-  }
-
-  var mult = 1;
-
-  var splitStr = num.toString().split("");
-
-  for (var i = 0; i < splitStr.length; i++) {
-
-    mult *= parseFloat(splitStr[i])
-
-  }
-
-  return 1 + persistence(parseFloat(mult));
-
-}
-
-
-
 // 7kyu-find-the-next-perfect-square
 
 
