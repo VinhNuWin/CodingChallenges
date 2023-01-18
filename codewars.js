8b115212983042c25327a04718bee213408dc666
Vinh Nguyen <vinhnuwin@protonmail.com>
2023-01-18 09:26:37 -0800

    6kyu tribonnaci sequence

--- codewars.js
+++ codewars.js
@@ -2,27 +2,6 @@
 
 
 // 6kyu-tribonacci-sequence
-
-
-
-
-
-function tribonacci(signature,n) {
-
-  var trib = signature;
-
-  for (i = 3; i < n; i++) {
-
-    trib.push((trib[i-1] + trib[i-2] + trib[i-3]));
-
-  }
-
-  return trib.slice(0, n);
-
-}
-
-
-
 // 8kyu-sort-and-star
 
 
