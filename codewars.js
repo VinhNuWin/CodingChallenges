0288bb01853f362ceb69e0dc5b77fa36300b9968
Vinh Nguyen <vinhnuwin@protonmail.com>
2023-01-20 09:59:01 -0800

    8kyu students final grade

--- codewars.js
+++ codewars.js
@@ -1,29 +1,3 @@
-// 8kyu-students-final-grade
-
-
-
-function finalGrade(exam, projects) {
-
-  if (exam > 90 || projects > 10) {
-
-    return 100;
-
-  } else if (exam > 75 && projects >= 5) {
-
-    return 90;
-
-  } else if (exam > 50 && projects >= 2) {
-
-    return 75;
-
-  }
-
-  return 0;
-
-}
-
-
-
 // 6kyu-array.diff
 
 
