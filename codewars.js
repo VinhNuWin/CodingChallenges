22f2985331061920dacb7ef3397de231b1ec0730
Vinh Nguyen <vinhnuwin@protonmail.com>
2023-01-28 10:02:09 -0800

    6kyu are they the same

--- codewars.js
+++ codewars.js
@@ -1,18 +1,3 @@
-// 6kyu-are-they-the-same 
-
-
-
-function comp(array1, array2) {
-
-  if(array1 == null || array2 == null) return false;
-
-  array1.sort((a, b) => a - b); array2.sort((a, b) => a - b);
-
-  return array1.map(v => v * v).every((v, i) => v == array2[i]);
-
-}
-
-
 
 // 8kyu-you-only-need-one
 
