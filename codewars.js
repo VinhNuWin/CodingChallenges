ea822ab4b959a323f0af22c3f9ee9acec6074da7
Vinh Nguyen <VinhNuwin@protonmail.com>
2023-02-14 09:14:40 -0800

    6kyu your order please

--- codewars.js
+++ codewars.js
@@ -1,21 +1,4 @@
 
-
-// 6kyu-your-order-please
-
-function order(words){
-  let arr = words.split(' ')
-  let r = []
-
-  arr.forEach(word=>{
-    let x = word.split('')
-    let num = x.find(el=>parseInt(el))
-    r.push([word,parseInt(num)])
-  })
-
-  r.sort((a,b)=>a[1]-b[1]).map(x=> x.splice(1,1))
-  return r.join(' ')
-}
-
 // 7kyu-Convert-an-array-of-strings-to-array-of-numbers
 
 function toNumberArray(stringarray){
