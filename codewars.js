f0cc5b0d9d50a1fc8b48f42d5b6d30318b00d4a8
Vinh Nguyen <VinhNuwin@protonmail.com>
2023-02-13 23:26:40 -0800

    6 kyu supermarket queue

--- codewars.js
+++ codewars.js
@@ -1,20 +1,4 @@
 
-// 6kyu-the-supermarket-queue
-
-function queueTime(customers, n) {
-  // creates an array of length n representing the tills
-  // Each till is given a value of 0 to represent initial waitTime before the queue begins.
-  const tills = new Array(n).fill(0);
-  // goes through the queue of customer waitTimes
-  for (let waitTime of customers) {
-    // finds the till with the least wait time on it, adds the next customer's time to it
-    const lowestWaitTill = tills.indexOf(Math.min(...tills));
-    tills[lowestWaitTill] += waitTime;
-  }
-  // end result is that the waitTimes (load) on the tills are distributed optimally.
-  // The waitTime of the till with the heaviest load represents the total time taken
-  return Math.max(...tills);
-}
 
 // 6kyu-your-order-please
 
