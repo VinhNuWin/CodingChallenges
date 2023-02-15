126f351071e32abf6884b6c529094b2f869b428e
Vinh Nguyen <VinhNuwin@protonmail.com>
2023-02-15 11:45:34 -0800

    7kyu convert an array of strings to array of numbers

--- codewars.js
+++ codewars.js
@@ -1,11 +1,3 @@
-
-// 7kyu-Convert-an-array-of-strings-to-array-of-numbers
-
-function toNumberArray(stringarray){
-
-  return stringarray.map(parseFloat);
-}
-
 // 6kyu-break-camelCase
 
 function solution(string) {
