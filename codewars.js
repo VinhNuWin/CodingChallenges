460cddf36f2bd52ca3f5853646adc8fca357e4fc
Vinh Nguyen <VinhNuwin@protonmail.com>
2023-03-13 05:10:56 -0700

    6kyu counting duplicates

--- codewars.js
+++ codewars.js
@@ -1,30 +1,3 @@
-
-  //5kyu maximum subarray sum
-
-  var maxSequence = function(arr) {
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++)
-      for (let j = 1; j <= arr.length; j++) {
-        if (arr.slice(i, j).reduce((a, b) => a + b, 0) > sum) {
-          sum = arr.slice(i, j).reduce((a, b) => a + b, 0);
-        }
-      }
-    return sum > 0 ? sum : 0;
-  };
-
-  //6kyu counting duplicates
-
-  function duplicateCount(text) {
-    return (
-      text
-        .toLowerCase()
-        .split("")
-        .sort()
-        .join("")
-        .match(/([^])\1+/g) || []
-    ).length;
-  }
-
   //8kyu name shuffler
 
   function nameShuffler(str) {
