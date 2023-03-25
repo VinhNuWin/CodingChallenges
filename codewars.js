f5c99e476f20ec51c599554f3aa476e3652588f4
Vinh Nguyen <VinhNuwin@protonmail.com>
2023-03-25 14:12:02 -0700

    8kyu super duper easy

--- codewars.js
+++ codewars.js
@@ -1,1 +1,17 @@
+//8kyu-reverse-list-order
 
+function reverseList(list) {
+    return list.reverse();
+  }
+
+//6kyu-duplicate-encoder
+
+function duplicateEncode(word) {
+    return word
+      .toLowerCase()
+      .split("")
+      .map(function(v, i, arr) {
+        return arr.indexOf(v) == arr.lastIndexOf(v) ? "(" : ")";
+      })
+      .join("");
+  }
