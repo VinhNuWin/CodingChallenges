386b77e7ba1db35f3567a941939c04a1d485cfe9
Vinh Nguyen <VinhNuwin@protonmail.com>
2023-03-27 17:53:29 -0700

    8kyu super duper easy

--- codewars.js
+++ codewars.js
@@ -15,3 +15,9 @@ function duplicateEncode(word) {
       })
       .join("");
   }
+
+//8kyu Exclamation marks series #1_ Remove a exclamation mark from the end of string.js
+
+function remove(s){
+    return s.replace(/!$/,'');
+   }
