67c87bb5819e8960981aa06106c4ab758d6b5a7b
Vinh Nguyen <VinhNuwin@protonmail.com>
2023-03-29 15:45:57 -0700

    6kyu duplicate encoder

--- codewars.js
+++ codewars.js
@@ -1,14 +1,4 @@
-//6kyu-duplicate-encoder
 
-function duplicateEncode(word) {
-    return word
-      .toLowerCase()
-      .split("")
-      .map(function(v, i, arr) {
-        return arr.indexOf(v) == arr.lastIndexOf(v) ? "(" : ")";
-      })
-      .join("");
-  }
 
 //8kyu Exclamation marks series #1_ Remove a exclamation mark from the end of string.js
 
