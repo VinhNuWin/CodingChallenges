30cdfa5395a8d3cbf695729592b48480d537c41e
Vinh Nguyen <VinhNuwin@protonmail.com>
2023-04-08 11:50:49 -0700

    8kyu cat years, dog years

--- codewars.js
+++ codewars.js
@@ -1,11 +1,3 @@
-  //8kyu cat years, dog years
-
-  const humanYearsCatYearsDogYears = humanYears => [
-    humanYears,
-    (humanYears - 1 ? 16 : 11) + 4 * humanYears,
-    (humanYears - 1 ? 14 : 10) + 5 * humanYears
-  ];
-
   //8kyu Vowel remover
 
   function shortcut(string){
