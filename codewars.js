2e7d63dee715902ac4043c2599687d6c7c731731
Vinh Nguyen <VinhNuwin@protonmail.com>
2023-04-10 05:12:39 -0700

    7kyu summing a number's digits

--- codewars.js
+++ codewars.js
@@ -1,5 +1,26 @@
-  //8kyu Vowel remover
 
-  function shortcut(string){
-    return string.replace(/[aeiou]/g,'')
+//8kyu GrassHopper - messi goals function
+
+function goals () {
+  return [...arguments].reduce((a,b)=>a+b,0);
 }
+
+//8kyu sum without highest and lowest numbers
+
+function sumArray(array) {
+  if (!Array.isArray(array)) return 0
+ return array
+     .sort((a, b) => a - b)
+     .slice(1, array.length - 1)
+     .reduce((a, b) => a + b, 0)
+ }
+
+ //8kyu total amount of points
+
+ const points=games=>games.reduce((output,current)=>{
+  return output += current[0]>current[2] ? 3 : current[0]===current[2] ? 1 : 0;
+},0)
+
+//8kyu beginner series #4 cockroach
+
+const cockroachSpeed = s => Math.floor(s / 0.036);
