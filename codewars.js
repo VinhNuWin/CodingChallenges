0b2fe36bc52c2bc6b6c3a96d53a0f89cfc985ef5
Vinh Nguyen <VinhNuwin@protonmail.com>
2023-04-12 18:26:33 -0700

    8kyu sum without highest and lowest numbers

--- codewars.js
+++ codewars.js
@@ -1,15 +1,3 @@
-
-
-//8kyu sum without highest and lowest numbers
-
-function sumArray(array) {
-  if (!Array.isArray(array)) return 0
- return array
-     .sort((a, b) => a - b)
-     .slice(1, array.length - 1)
-     .reduce((a, b) => a + b, 0)
- }
-
  //8kyu total amount of points
 
  const points=games=>games.reduce((output,current)=>{
