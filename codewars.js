e5cfc8c1d817a5da4c43310b7f38debc1f2d5f7a
Vinh Nguyen <VinhNuwin@protonmail.com>
2023-04-13 17:56:37 -0700

    8kyu total amount of points

--- codewars.js
+++ codewars.js
@@ -1,8 +1,4 @@
- //8kyu total amount of points
 
- const points=games=>games.reduce((output,current)=>{
-  return output += current[0]>current[2] ? 3 : current[0]===current[2] ? 1 : 0;
-},0)
 
 //8kyu beginner series #4 cockroach
 
