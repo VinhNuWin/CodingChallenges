751107712add82bc3355026700c5771c967e4b57
Vinh Nguyen <VinhNuwin@protonmail.com>
2023-04-20 05:09:11 -0700

    8kyu Training JS #2: Basic data types--Number

--- codewars.js
+++ codewars.js
@@ -1,12 +1,4 @@
 
-  //8kyu Training JS #2: Basic data types--Number
-
-  var v1=50;v2=100,v3=150,v4=200,v5=2,v6=250
-function equal1(){
-  var a=v1   
-  var b=v1   
-  return a+b;
-}
 //Please refer to the example above to complete the following functions
 function equal2(){
   var a= v3  //set number value to a
