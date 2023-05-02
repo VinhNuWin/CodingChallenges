444efc145389dfb93d8047f26bc72ec397a3e657
Vinh Nguyen <VinhNuwin@protonmail.com>
2023-05-02 05:21:22 -0700

    7kyu remove everything from anchor url

---
+++ codewars.js
@@ -0,0 +1,35 @@
+
+
+//8kyu-how-many-lightsabers-do-you-own
+
+function howManyLightsabersDoYouOwn(name){
+  return name=="Zach"?18:0;
+  
+  }
+
+  //7kyu-anagram-detection
+
+  function isAnagram(test, original) {
+    return (
+      test
+        .toLowerCase()
+        .split("")
+        .sort()
+        .join("") ===
+      original
+        .toLowerCase()
+        .split("")
+        .sort()
+        .join("")
+    );
+  }
+
+  //8kyu-will-you-make-it
+
+  const zeroFuel = (distanceToPump, mpg, fuelLeft) => mpg * fuelLeft >= distanceToPump;
+
+  //8kyu-enumerable-magic-#25-take-the-first-n-elements
+
+  function take(arr, n) {
+    return arr.slice(0, n);
+  }
