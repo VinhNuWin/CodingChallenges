69c5f8e56e7c71c7f7e2557d535568ea2f2caa04
Vinh Nguyen <VinhNuwin@protonmail.com>
2023-05-05 19:23:46 -0700

    8kyu will you make it

--- codewars.js
+++ codewars.js
@@ -1,28 +1,5 @@
 
 
-
-
-  //7kyu-anagram-detection
-
-  function isAnagram(test, original) {
-    return (
-      test
-        .toLowerCase()
-        .split("")
-        .sort()
-        .join("") ===
-      original
-        .toLowerCase()
-        .split("")
-        .sort()
-        .join("")
-    );
-  }
-
-  //8kyu-will-you-make-it
-
-  const zeroFuel = (distanceToPump, mpg, fuelLeft) => mpg * fuelLeft >= distanceToPump;
-
   //8kyu-enumerable-magic-#25-take-the-first-n-elements
 
   function take(arr, n) {
