707ec338870d2536e5c7f7c717f20817943b050d
Vinh Nguyen <VinhNuwin@protonmail.com>
2023-05-29 17:48:18 -0700

    7kyu find the stray number

--- codewars.js
+++ codewars.js
@@ -1,12 +1,6 @@
 
 
-//7kyu-find-the-stray-number
 
-function stray(numbers) {
-  return numbers.find(
-    number => numbers.indexOf(number) === numbers.lastIndexOf(number)
-  );
-}
 
 //8kyu-sentence-smash
 
