96548bfe2b005c4306b31bdfe905a5674ff1795a
Vinh Nguyen <VinhNuwin@protonmail.com>
2023-06-27 20:47:10 -0700

    8kyu hello name or world

--- codewars.js
+++ codewars.js
@@ -1,10 +1,39 @@
-//7kyu-find-the-middle-element
-var gimme = function(inputArray) {
-    return [...inputArray].indexOf(inputArray.sort((a, b) => a - b)[1]);
-  };
-
-
 
   
  
+//6kyu-playing-with-digits
+
+function digPow(n, p) {
+  let arr =
+    n
+      .toString()
+      .split("")
+      .map(Number)
+      .reduce((acc, cur, i, arr) => acc + Math.pow(arr[i], p + i), 0) / n;
+  return ("" + arr).includes(".") ? -1 : arr;
+}
+
+//8kyu-find-the-first-non-consecutive-number
+
+function firstNonConsecutive(arr) {
+  const result = arr.find((number, index) => number !== index + arr[0])
+  return Number.isInteger(result) ? result : null
+}
+
+//8kyu-opposites-attract
+
+function lovefunc(flower1, flower2){
+  return flower1 % 2 !== flower2 % 2;
+}
+
+//8kyu-sum-without-highest-and-lowest-numbers
+
+function sumArray(array) {
+  if (!Array.isArray(array)) return 0
+ return array
+     .sort((a, b) => a - b)
+     .slice(1, array.length - 1)
+     .reduce((a, b) => a + b, 0)
+ }
+
 
