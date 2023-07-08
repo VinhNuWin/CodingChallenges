a7856ef1d65f7cc5b5d3ab53a61b567af23222f8
Vinh Nguyen <VinhNuwin@protonmail.com>
2023-07-07 18:46:46 -0700

    6kyu playing with digits

--- codewars.js
+++ codewars.js
@@ -26,14 +26,5 @@ function lovefunc(flower1, flower2){
   return flower1 % 2 !== flower2 % 2;
 }
 
-//8kyu-sum-without-highest-and-lowest-numbers
-
-function sumArray(array) {
-  if (!Array.isArray(array)) return 0
- return array
-     .sort((a, b) => a - b)
-     .slice(1, array.length - 1)
-     .reduce((a, b) => a + b, 0)
- }
 
 
