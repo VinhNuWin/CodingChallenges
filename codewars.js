81779f3f4599aa3c3764c5bd83e69ee5a6c19414
Vinh Nguyen <VinhNuwin@protonmail.com>
2023-07-09 00:15:58 -0700

    find the first non consecutive number

--- codewars.js
+++ codewars.js
@@ -1,24 +1,4 @@
 
-  
- 
-//6kyu-playing-with-digits
-
-function digPow(n, p) {
-  let arr =
-    n
-      .toString()
-      .split("")
-      .map(Number)
-      .reduce((acc, cur, i, arr) => acc + Math.pow(arr[i], p + i), 0) / n;
-  return ("" + arr).includes(".") ? -1 : arr;
-}
-
-//8kyu-find-the-first-non-consecutive-number
-
-function firstNonConsecutive(arr) {
-  const result = arr.find((number, index) => number !== index + arr[0])
-  return Number.isInteger(result) ? result : null
-}
 
 //8kyu-opposites-attract
 
