84497876a3806970efbe8b448f0bf5db2b2cf85e
Vinh Nguyen <VinhNuwin@protonmail.com>
2023-09-05 18:12:40 -0700

    8kyu do you speak english

--- codewars.js
+++ codewars.js
@@ -1,5 +1,30 @@
-//8kyu-do-you-speak-english
+//8kyu-if-you-cant-sleep-just-count-sheep
 
-function spEng(s) {
-  return /english/i.test(s);
+var countSheep = function (num) {
+  let str = "";
+  for (let i = 1; i <= num; i++) {
+    str += `${i} sheep...`;
+  }
+  return str;
+};
+
+//6kyu-title-case
+
+function titleCase(title, minorWords) {
+  var minorWords =
+    typeof minorWords !== "undefined"
+      ? minorWords.toLowerCase().split(" ")
+      : [];
+  return title
+    .toLowerCase()
+    .split(" ")
+    .map(function (v, i) {
+      if (v != "" && (minorWords.indexOf(v) === -1 || i == 0)) {
+        v = v.split("");
+        v[0] = v[0].toUpperCase();
+        v = v.join("");
+      }
+      return v;
+    })
+    .join(" ");
 }
