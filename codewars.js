cf653e860fbf3f30b9dfe01798270fc05d454765
Vinh Nguyen <VinhNuwin@protonmail.com>
2023-09-27 21:17:13 -0700

    8kyu-fake-binary

--- codewars.js
+++ codewars.js
@@ -5,3 +5,17 @@ function helloWorld() {
   console.log("Hello World!");
   ("Hello World!");
 }
+
+// 7kyu-check-the-exam
+
+const checkExam = (answers, responses) => {
+  const score = responses.reduce((total, response, index) => {
+    if (!response) return total;
+    if (response === answers[index]) return total + 4;
+    if (response !== answers[index]) return total - 1;
+
+    return total;
+  }, 0);
+
+  return score > 0 ? score : 0;
+};
