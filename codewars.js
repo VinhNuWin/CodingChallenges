6316555712f3e85c2cbe3f3e9f99bb102f01a676
Vinh Nguyen <VinhNuwin@protonmail.com>
2023-11-02 18:29:19 -0700

    formatting decimal places

--- codewars.js
+++ codewars.js
@@ -1,10 +1,4 @@
-//8kyu Formatting decimal places #0
-
-function twoDecimalPlaces(n) {
-  return +n.toFixed(2);
-}
-
-//8kyu Determine offspring sex based on genes XX and XY chromosomes
+//8kyu-Determine-offspring-sex-based-on-genes-XX-and-XY-chromosomes
 
 function chromosomeCheck(sperm) {
   return `Congratulations! You're going to have a ${
@@ -12,7 +6,7 @@ function chromosomeCheck(sperm) {
   }.`;
 }
 
-//8kyu Regex count lowercase letters
+//8kyu-Regex-count-lowercase-letters
 
 function lowercaseCount(str) {
   return (str.match(/[a-z]/g) || []).length;
