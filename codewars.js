9a8f29aa6739366dee99881adc5179647674f100
Vinh Nguyen <VinhNuwin@protonmail.com>
2023-11-07 17:48:31 -0800

    8kyu determine offspring sex based on genes xx and xy chromosomes

--- codewars.js
+++ codewars.js
@@ -1,11 +1,3 @@
-//8kyu-Determine-offspring-sex-based-on-genes-XX-and-XY-chromosomes
-
-function chromosomeCheck(sperm) {
-  return `Congratulations! You're going to have a ${
-    sperm === "XY" ? "son" : "daughter"
-  }.`;
-}
-
 //8kyu-Regex-count-lowercase-letters
 
 function lowercaseCount(str) {
