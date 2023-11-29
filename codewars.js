03f4a7015f219aca7979ade948f0020952963639
Vinh Nguyen <VinhNuwin@protonmail.com>
2023-11-28 16:33:41 -0800

    5kyu Integers: Recreation-One

--- codewars.js
+++ codewars.js
@@ -1,8 +1,18 @@
-//7kyu-mumbling
+//7kyu-No-Oddities-Here
 
-function accum(s) {
-  return s
-    .split("")
-    .map((c, i) => c.toUpperCase() + c.toLowerCase().repeat(i))
-    .join("-");
+var noOdds = (values) => values.filter((x) => x % 2 === 0);
+
+//7kyu-the-coupon-code
+
+function checkCoupon(enteredCode, correctCode, currentDate, expirationDate) {
+  return (
+    enteredCode === correctCode &&
+    Date.parse(expirationDate) >= Date.parse(currentDate)
+  );
+}
+
+//8kyu-Is-It-Even?
+
+function testEven(n) {
+  return n % 2 == 0;
 }
