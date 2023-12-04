62c062e75f3391a025dcd7c0e557726d4fcf6ecc
Vinh Nguyen <VinhNuwin@protonmail.com>
2023-12-03 21:23:16 -0800

    7kyu-the-coupon-code

--- codewars.js
+++ codewars.js
@@ -1,12 +1,3 @@
-//7kyu-the-coupon-code
-
-function checkCoupon(enteredCode, correctCode, currentDate, expirationDate) {
-  return (
-    enteredCode === correctCode &&
-    Date.parse(expirationDate) >= Date.parse(currentDate)
-  );
-}
-
 //8kyu-Is-It-Even?
 
 function testEven(n) {
